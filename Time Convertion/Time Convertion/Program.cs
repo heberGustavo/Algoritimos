53252731657edd2ad3655af0cbac3581ebcee519
heberGustavo <heber.gbarbosa@hotmail.com>
2023-03-23 08:05:49 -0300

    Criação de projeto Time Convertion

---
+++ Time Convertion/Time Convertion/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Time_Convertion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
